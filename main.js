59a7741cf35d038444f513e3d0f49b56eef48aa0
MimiTranLam <75051613+MimiTranLam@users.noreply.github.com>
2021-10-04 10:05:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-const sections = document.querySelectorAll('section');
-const indicator = document.querySelector('.indicator');
-
-const options = {
-    threshold: 0.7
-};
-
-let observer = new IntersectionObserver(navCheck, options);
-
-function navCheck(entries) {
-    entries.forEach(entry => {
-    let className = entry.target.className;
-    console.log(className);
-    });
-}
-
-sections.forEach(section => {
-    observer.observe(section)  
-});
-
-function top() {
-    window.location.href = "dev.html";
+var a;
+function languages() {
+  if (a == 1){
+    document.getElementById(nav-bottom).style.display="inline";
+    return a=0;
+  }
+  else {
+    document.getElementById("nav-bottom").style.display="none";
+    return a=1;
+  }
 }
